8126d5448415e39067e63a9c0a35cc6a0d226b43
nixonyung <nixonyung123@gmail.com>
2024-11-17 11:40:24 +0800

    refactor scripts

--- docker/scripts/make_submission.py
+++ docker/scripts/make_submission.py
@@ -5,33 +5,35 @@ from typing import TextIO
 
 INCLUDE_DIR = Path("/repo/include")
 OUT_DIR = Path("/out")
+OUT_FILE = OUT_DIR / "submission.cpp"
 
 
-included_header_files = set[str]()
-
-
-def write_file(
+def _write_file(
     src_path: Path,
-    out_file: TextIO,
+    out_fp: TextIO,
+    included_header_files: set[str],
 ):
     for line in src_path.read_text().splitlines():
-        if (m := re.fullmatch(r"#include \"(?P<header_file>.*)\"", line)) is not None:
-            header_file = m.group("header_file")
-            if header_file not in included_header_files:
-                write_file(
-                    src_path=INCLUDE_DIR / m.group("header_file"),
-                    out_file=out_file,
+        if (m := re.fullmatch(r"#include \"(?P<header_filename>.*)\"", line)) is not None:
+            header_filename = str(m.group("header_filename"))
+            if header_filename not in included_header_files:
+                _write_file(
+                    src_path=INCLUDE_DIR / header_filename,
+                    out_fp=out_fp,
+                    included_header_files=included_header_files,
                 )
-                included_header_files.add(header_file)
+                included_header_files.add(header_filename)
         else:
-            fp.write(line)
-            fp.write("\n")
-    fp.write("\n")
+            out_fp.write(line)
+            out_fp.write("\n")
+    out_fp.write("\n")
+
+
+def write_file(src_path: Path):
+    included_header_files = set[str]()
+    with (OUT_DIR / "submission.cpp").open("w") as fp:
+        _write_file(src_path=src_path, out_fp=fp, included_header_files=included_header_files)
 
 
 if __name__ == "__main__":
-    with (OUT_DIR / "submission.cpp").open("w") as fp:
-        write_file(
-            src_path=Path(sys.argv[1]),
-            out_file=fp,
-        )
+    write_file(src_path=Path(sys.argv[1]))
--- docker/scripts/visualize_graph.py
+++ docker/scripts/visualize_graph.py
@@ -1,34 +1,36 @@
 import sys
 from pathlib import Path
 
-import graphviz
+import graphviz  # type:ignore
 
 OUT_DIR = Path("/out")
 
-g = graphviz.Digraph(  # `graphviz.Graph` for undirected graph
-    name="graph",
-    format="png",
-    graph_attr={
-        "bgcolor": "black",
-    },
-    node_attr={
-        "color": "white",
-        "fontcolor": "white",
-        "overlap": "scale",
-    },
-    edge_attr={
-        "color": "white",
-        "fontcolor": "white",
-        "splines": "line",
-    },
-)
 
 if __name__ == "__main__":
+    g = graphviz.Digraph(
+        name="graph",
+        format="png",
+        graph_attr={
+            "bgcolor": "black",
+        },
+        node_attr={
+            "color": "white",
+            "fontcolor": "white",
+            "overlap": "scale",  # (ref.) [laying out a large graph with graphviz](https://stackoverflow.com/questions/13417411/laying-out-a-large-graph-with-graphviz)
+        },
+        edge_attr={
+            "color": "white",
+            "fontcolor": "white",
+        },
+    )
+
     for edge in Path(sys.argv[1]).read_text().splitlines()[1:]:
         tokens = edge.split()
-        match len(tokens):
-            case 2:
-                g.edge(str(int(tokens[0]) - 1), str(int(tokens[1]) - 1))
-            case 3:
-                g.edge(str(int(tokens[0]) - 1), str(int(tokens[1]) - 1), xlabel=tokens[2])
+        src = str(int(tokens[0]) - 1)
+        dest = str(int(tokens[1]) - 1)
+        weight = None
+        if len(tokens) >= 3:
+            weight = tokens[2]
+
+        g.edge(src, dest, xlabel=weight)
     g.render(directory=OUT_DIR)
